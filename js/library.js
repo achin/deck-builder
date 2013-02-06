5c3fd9a73dc78141483b25e999a97731454f12d9
Alex Chin <alex.ba.chin@gmail.com>
2013-02-05 20:54:11 -0500

    Fixed filters.

--- js/library.js
+++ js/library.js
@@ -1,5 +1,22 @@
-function LibraryCtrl($scope, $http) {
+var app = angular.module("deck-builder", ["ui"]);
+
+app.controller("LibraryCtrl", function LibraryCtrl($scope, $http) {
     $http.get("library.json").success(function(data) {
         $scope.library = data;
     });
-}
+
+    $scope.filterLibrary = function(card) {
+        return _($scope.filters)
+            .map(function(filter) {
+                return filter.split(":")
+            })
+            .groupBy(_.head)
+            .every(function(pairs) {
+                return _.some(pairs, function(pair) {
+                    var attr = pair[0], val = pair[1];
+                    return card[attr] === val;
+                });
+            })
+            .valueOf();
+    }
+});
